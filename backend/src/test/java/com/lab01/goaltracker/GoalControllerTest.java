21572710ea6471f1e667f41ef955040f2b0f8e1a
hkcodebase <hkchauhan022@gmail.com>
2024-04-12 17:55:49 -0400

    removed unnecessary import

--- backend/src/test/java/com/lab01/goaltracker/GoalControllerTest.java
+++ backend/src/test/java/com/lab01/goaltracker/GoalControllerTest.java
@@ -8,13 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.time.LocalDateTime;
 
 import static org.hamcrest.Matchers.*;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
